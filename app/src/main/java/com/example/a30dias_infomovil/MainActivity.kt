60a6c561be368841bc3c004b5113031ad32345e8
magilssa <magilssa@unirioja.es>
2024-02-25 18:10:14 +0100

    Initial commit  - Fallan las sombras que los CantanteItem y faltan de poner las fotos de cada cantante

---
+++ app/src/main/java/com/example/a30dias_infomovil/MainActivity.kt
@@ -0,0 +1,187 @@
+package com.example.a30dias_infomovil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30dias_infomovil.data.Cantante
+import com.example.a30dias_infomovil.data.cantantes
+import com.example.a30dias_infomovil.ui.theme._30Dias_INFOMOVILTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _30Dias_INFOMOVILTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    CantantesApp()
+                }
+            }
+        }
+    }
+}
+
+//APP PRINCIPAL
+
+@Composable
+fun CantantesApp(){
+    Scaffold(
+        topBar = {
+            CantantesTopBar()
+        }
+    ){ it ->
+        LazyColumn(
+            contentPadding = it
+        ){
+            items(cantantes){
+                CantanteItem(
+                    cantante = it,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+//PARTES
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CantantesTopBar(modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(64.dp)
+                        .padding(8.dp),
+                    painter = painterResource(R.drawable.ic_launcher_foreground),
+                    contentDescription = null
+                )
+                Text(
+                    text = "30 DIAS",
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun CantanteItem(
+    cantante: Cantante,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = Modifier
+            .aspectRatio(0.8F)
+            .shadow(elevation = 12.dp, shape = RectangleShape)
+            .border(24.dp, color = Color.White, shape = RectangleShape)
+    ){
+        CantanteDia(cantante.dia)
+        CantanteFoto(cantante.imageResourceId)
+        CantanteInfo(cantante.name,cantante.descrp)
+    }
+}
+
+@Composable
+fun CantanteDia(
+    diaa: String,
+    modifier: Modifier = Modifier
+){
+    Text(
+        text = diaa,
+        style = MaterialTheme.typography.displayMedium,
+        modifier = Modifier.padding(8.dp)
+            .padding(top = 0.dp)
+    )
+}
+
+@Composable
+fun CantanteFoto(
+    @DrawableRes cantanteIcon: Int,
+    modifier: Modifier = Modifier
+){
+    Image(
+        modifier = modifier
+            .size(64.dp)
+            .padding(8.dp)
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop,
+        painter = painterResource(cantanteIcon),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun CantanteInfo(
+    @StringRes namee:Int,
+    @StringRes descrip:Int,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+    ){
+        Text(
+            text = stringResource(namee),
+            modifier = Modifier.padding(24.dp),
+            style = MaterialTheme.typography.displaySmall,
+        )
+        Text(
+            text = stringResource(descrip),
+            modifier = Modifier.padding(24.dp)
+        )
+    }
+}
+
+//VISUALIZACIÓN EN PANTALLA (tema claro y oscuro)
+@Preview()
+@Composable
+fun CantantesPreview() {
+    _30Dias_INFOMOVILTheme {
+        CantantesApp()
+    }
+}
+
+@Preview()
+@Composable
+fun CantantesDarkThemePreview() {
+    _30Dias_INFOMOVILTheme (darkTheme = true){
+        CantantesApp()
+    }
+}
---
+++ app/src/main/java/com/example/a30dias_infomovil/data/Cantante.kt
@@ -0,0 +1,46 @@
+package com.example.a30dias_infomovil.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.a30dias_infomovil.R
+
+data class Cantante(
+    val dia: String,
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    @StringRes val descrp: Int,
+)
+
+val cantantes = listOf(
+    Cantante("Dia 1", R.drawable.ic_launcher_foreground,R.string.nombre1,R.string.descripcionGeneral),
+    Cantante("Dia 2", R.drawable.ic_launcher_foreground,R.string.nombre2,R.string.descripcionGeneral),
+    Cantante("Dia 3", R.drawable.ic_launcher_foreground,R.string.nombre3,R.string.descripcionGeneral),
+    Cantante("Dia 4", R.drawable.ic_launcher_foreground,R.string.nombre4,R.string.descripcionGeneral),
+    Cantante("Dia 5", R.drawable.ic_launcher_foreground,R.string.nombre5,R.string.descripcionGeneral),
+    Cantante("Dia 6", R.drawable.ic_launcher_foreground,R.string.nombre6,R.string.descripcionGeneral),
+    Cantante("Dia 7", R.drawable.ic_launcher_foreground,R.string.nombre7,R.string.descripcionGeneral),
+    Cantante("Dia 8", R.drawable.ic_launcher_foreground,R.string.nombre8,R.string.descripcionGeneral),
+    Cantante("Dia 9", R.drawable.ic_launcher_foreground,R.string.nombre9,R.string.descripcionGeneral),
+    Cantante("Dia 10", R.drawable.ic_launcher_foreground,R.string.nombre10,R.string.descripcionGeneral),
+    Cantante("Dia 11", R.drawable.ic_launcher_foreground,R.string.nombre11,R.string.descripcionGeneral),
+    Cantante("Dia 12", R.drawable.ic_launcher_foreground,R.string.nombre12,R.string.descripcionGeneral),
+    Cantante("Dia 13", R.drawable.ic_launcher_foreground,R.string.nombre13,R.string.descripcionGeneral),
+    Cantante("Dia 14", R.drawable.ic_launcher_foreground,R.string.nombre14,R.string.descripcionGeneral),
+    Cantante("Dia 15", R.drawable.ic_launcher_foreground,R.string.nombre15,R.string.descripcionGeneral),
+    Cantante("Dia 16", R.drawable.ic_launcher_foreground,R.string.nombre16,R.string.descripcionGeneral),
+    Cantante("Dia 17", R.drawable.ic_launcher_foreground,R.string.nombre17,R.string.descripcionGeneral),
+    Cantante("Dia 18", R.drawable.ic_launcher_foreground,R.string.nombre18,R.string.descripcionGeneral),
+    Cantante("Dia 19", R.drawable.ic_launcher_foreground,R.string.nombre19,R.string.descripcionGeneral),
+    Cantante("Dia 20", R.drawable.ic_launcher_foreground,R.string.nombre20,R.string.descripcionGeneral),
+    Cantante("Dia 21", R.drawable.ic_launcher_foreground,R.string.nombre21,R.string.descripcionGeneral),
+    Cantante("Dia 22", R.drawable.ic_launcher_foreground,R.string.nombre22,R.string.descripcionGeneral),
+    Cantante("Dia 23", R.drawable.ic_launcher_foreground,R.string.nombre23,R.string.descripcionGeneral),
+    Cantante("Dia 24", R.drawable.ic_launcher_foreground,R.string.nombre24,R.string.descripcionGeneral),
+    Cantante("Dia 25", R.drawable.ic_launcher_foreground,R.string.nombre25,R.string.descripcionGeneral),
+    Cantante("Dia 26", R.drawable.ic_launcher_foreground,R.string.nombre26,R.string.descripcionGeneral),
+    Cantante("Dia 27", R.drawable.ic_launcher_foreground,R.string.nombre27,R.string.descripcionGeneral),
+    Cantante("Dia 28", R.drawable.ic_launcher_foreground,R.string.nombre28,R.string.descripcionGeneral),
+    Cantante("Dia 29", R.drawable.ic_launcher_foreground,R.string.nombre29,R.string.descripcionGeneral),
+    Cantante("Dia 30", R.drawable.ic_launcher_foreground,R.string.nombre30,R.string.descripcionGeneral),
+
+)
